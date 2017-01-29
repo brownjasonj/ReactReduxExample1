8e1fe219e8d9a20c5c2c2617a68e8360ac6211b7
Jason Brown <jason@Jasons-MacBook.local>
2017-01-29 07:32:04 +0100

    Initial version of example code

---
+++ src/actions/index.tsx
@@ -0,0 +1,3 @@
+export default function selectBook(book: any) {
+    console.log('A book has been selected:', book.title);
+}
---
+++ src/containers/book-list.container.tsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import * as ReactRedux from 'react-redux';
+import * as Redux from 'redux';
+
+import selectBook from '../actions/index';
+
+class BookList extends React.Component<any, any> {
+    constructor(props: any) {
+        super(props);
+    }
+
+    private renderList()  {
+        return this.props.books.map((book: any) => {
+            return (
+                <li key={book.title} 
+                    onClick={() => this.props.selectBook(book)}
+                    className="list-group-item">
+                {book.title}
+                </li>
+            );
+        });
+    }
+
+    public render() {
+        return (
+            <ul className="list-group col-sm-4">
+            {this.renderList()}
+            </ul>
+        );
+    }
+}
+
+// the mapStateToProps function takes an application state and 
+// selects a specific sub state for the a specific component
+// so functionally we have:
+//
+//      mapStateToProps: Application State => Container State
+// 
+// The ReactRedux framework will add the object items returned 
+// by this function to the props state of the container!
+//
+function mapStateToProps(state: any) {
+    // Whatever is returned will show up as props 
+    // inside of BookList
+    return {
+        books: state.books
+    };
+}
+
+// Anything returned from this function will end up as props 
+// on the BookList container.  As with the mapStateToProps this 
+// function in essence adds new object items(members) to the 
+// dispatch object passed in.
+// The ReactRedux framework will add the items in the dispatch
+// object to the props object of the container.
+// 
+function mapDispatchToProps(dispatch: any) {
+    // Whenever selectBook is called, the result should be passed 
+    // to all of our reducers  (that have been bound to it!)
+    return Redux.bindActionCreators({selectBook: selectBook}, dispatch);
+}
+
+// the Redux connect function in essence takes the app state to container state function
+// calls it to get the container state and then binds the contrainer state the props value 
+// of the container......conceptually very simple, but very complicated when expressed in code!
+// In addition, the 
+export default ReactRedux.connect(mapStateToProps, mapDispatchToProps)(BookList);
